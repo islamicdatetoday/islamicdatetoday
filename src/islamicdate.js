2d2a644bb69c15553d667a8f72425b3a47669683
islamicdatetoday <islamicdatetodayofficial@gmail.com>
2026-01-10 11:06:36 +0530

    Update islamicdate.js

--- src/islamicdate.js
+++ src/islamicdate.js
@@ -1,1 +1,19 @@
+export class IslamicDateEngine {
+  constructor(dataset) {
+    this.dataset = dataset;
+    this.index = new Map();
+
+    for (const row of dataset) {
+      this.index.set(row.gregorian_date, row);
+    }
+  }
+
+  getByGregorian(dateStr) {
+    return this.index.get(dateStr) || null;
+  }
+
+  getAll() {
+    return this.dataset;
+  }
+}
 
